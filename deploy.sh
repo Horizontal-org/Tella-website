f5eda145628540ec5fb5c5f18b524b49472a4f7e
juandans01 <juandans01@gmail.com>
2023-04-19 15:18:30 -0300

    add options to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,56 @@
-git fetch --all 
+#!/bin/bash
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "deploy docusaurus"
+   echo
+   echo "Syntax: deploy.sh -b branch_name"
+   echo "options:"
+   echo "b     add branch name [REQUIRED]"
+   echo "h     Print this Help."
+   echo
+}
 
-git reset --hard origin/main
+############################################################
+############################################################
+# Main program                                             #
+############################################################
+############################################################
 
-npm install
+unset Branch
 
-npm run build
+############################################################
+# Process the input options. Add options as needed.        #
+############################################################
+# Get the options
+while getopts ":hb:" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      b)
+         Branch=$OPTARG;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+
+if [ -z "$Branch" ]
+then
+      echo "Error: need a branch name"
+else
+      echo "Deploying $Branch"
+
+      git fetch --all 
+
+      git reset --hard origin/$Branch
+
+      npm install
+
+      npm run build
+fi
